a8ec4b1c9a5b73f49de35802fb140b419664e92e
hmoob lee <onlinegame136@gmail.com>
2021-06-14 07:27:18 +0700

    Update new

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ require('View/masster/head.php');
 if (isset($_GET['controller'])) {
 		require 'Route/admin/web.php'; /*xử lý các request trong Route/web.php*/
 	} else {
-		header('location:index.php?controller=Xl-clien'); /*require giao diện trang chủ*/
+		//header('location:index.php?controller=Xl-clien'); /*require giao diện trang chủ*/
+		require 'Controller/Clien/Xl-clien.php';
 	}
 
 require 'View/masster/footer.php'; /*giao diện footer*/
